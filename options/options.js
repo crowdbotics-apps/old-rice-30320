e0971922db9f6cb8e328c49e5050a3f49726b724
Crowdbotics <team@crowdbotics.com>
2021-09-05 03:18:30 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "old_rice_30320Identifier",
+  url: "https://old_rice_30320Identifier.botics.co",
+  api: "https://old_rice_30320Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
